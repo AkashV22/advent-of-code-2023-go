cf9280e3baadb23d8057e3d54c5a61baf6a59df4
AkashV22 <iakashv22@gmail.com>
2023-12-25 23:33:11 +0000

    Set up a HTTP server.

--- main.go
+++ main.go
@@ -1,5 +1,65 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
 
+	"github.com/AkashV22/advent-of-code-2023-go/day01"
+)
+
+type solverInfo struct {
+	day    int
+	puzzle int
+	solver func(*bufio.Scanner) (string, error)
+}
+
+func solvePuzzles(solvers []solverInfo) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		results := make(map[string]string)
+
+		for _, solverInfo := range solvers {
+			file, err := os.Open("day01/input1.txt")
+			defer file.Close()
+
+			lines := bufio.NewScanner(file)
+
+			result, err := solverInfo.solver(lines)
+
+			if err != nil {
+				slog.Error("Error solving puzzle.", err)
+				http.Error(w, err.Error(), 500)
+				return
+			}
+
+			results[fmt.Sprintf("Day %v Puzzle %v", solverInfo.day, solverInfo.puzzle)] = result
+		}
+
+		resultsJson, err := json.Marshal(results)
+
+		if err != nil {
+			slog.Error("Error mashalling results to JSON.", err)
+			http.Error(w, err.Error(), 500)
+			return
+		}
+
+		w.Write(resultsJson)
+
+	}
+}
+
+func main() {
+	solvers := []solverInfo{
+		{day: 1, puzzle: 1, solver: day01.SolvePuzzle1},
+	}
+
+	http.HandleFunc("/", solvePuzzles(solvers))
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		slog.Error("Error starting server, exiting...", err)
+		os.Exit(1)
+	}
 }
