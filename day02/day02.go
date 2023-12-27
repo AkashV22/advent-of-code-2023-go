835f0ac1cf7ea0e786b23d78cfa537313050a39a
AkashV22 <iakashv22@gmail.com>
2023-12-27 17:42:40 +0000

    Implemented a solution for day 2 puzzle 1.

--- day02/day02.go
+++ day02/day02.go
@@ -2,9 +2,35 @@ package day02
 
 import (
 	"bufio"
+	"log/slog"
+	"strconv"
+	"strings"
 
 	"github.com/AkashV22/advent-of-code-2023-go/puzzle"
 )
+
+func isGamePossible(cubeSets []string) bool {
+	for _, cubeSet := range cubeSets {
+		cubes := strings.Split(cubeSet, ", ")
+
+		for _, cube := range cubes {
+			cubeData := strings.Split(cube, " ")
+			total, err := strconv.Atoi(cubeData[0])
+			if err != nil {
+				slog.Error("Error converting cube total to int.", err)
+				return false
+			}
+
+			colour := cubeData[1]
+
+			if (colour == "red" && total > 12) || (colour == "green" && total > 13) || (colour == "blue" && total > 14) {
+				return false
+			}
+		}
+	}
+
+	return true
+}
 
 type puzzleSolver struct{}
 
@@ -17,7 +43,27 @@ func (solver *puzzleSolver) SolvePuzzle(puzzleNumber puzzle.Number, input *bufio
 		return 0
 	}
 
-	return 8
+	sum := 0
+
+	for input.Scan() {
+		game := strings.Replace(input.Text(), "Game ", "", 1)
+
+		gameData := strings.Split(game, ": ")
+
+		id, err := strconv.Atoi(gameData[0])
+		if err != nil {
+			slog.Error("Error converting ID to int.", err)
+			continue
+		}
+
+		cubeSets := strings.Split(gameData[1], "; ")
+
+		if isGamePossible(cubeSets) {
+			sum += id
+		}
+	}
+
+	return sum
 }
 
 func NewPuzzleSolver() puzzle.Solver {
