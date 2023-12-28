052a0c51f3d3f2524cb3cc6d43334e85b467c281
AkashV22 <iakashv22@gmail.com>
2023-12-28 01:42:27 +0000

    Implemented a solution for day 2 puzzle 2.

--- day02/day02.go
+++ day02/day02.go
@@ -2,13 +2,15 @@ package day02
 
 import (
 	"bufio"
+	"errors"
+	"fmt"
 	"strconv"
 	"strings"
 
 	"github.com/AkashV22/advent-of-code-2023-go/puzzle"
 )
 
-func isGamePossible(cubeSets []string) (bool, error) {
+func passThroughValueIfGameIsPossible(cubeSets []string, value int) (int, error) {
 	for _, cubeSet := range cubeSets {
 		cubes := strings.Split(cubeSet, ", ")
 
@@ -16,18 +18,47 @@ func isGamePossible(cubeSets []string) (bool, error) {
 			cubeData := strings.Split(cube, " ")
 			total, err := strconv.Atoi(cubeData[0])
 			if err != nil {
-				return false, err
+				return 0, err
 			}
 
 			colour := cubeData[1]
 
 			if (colour == "red" && total > 12) || (colour == "green" && total > 13) || (colour == "blue" && total > 14) {
-				return false, nil
+				return 0, nil
 			}
 		}
 	}
 
-	return true, nil
+	return value, nil
+}
+
+func calculatePowerOfCubes(cubeSets []string) (int, error) {
+	var maxRedTotal, maxGreenTotal, maxBlueTotal int
+
+	for _, cubeSet := range cubeSets {
+		cubes := strings.Split(cubeSet, ", ")
+
+		for _, cube := range cubes {
+			cubeData := strings.Split(cube, " ")
+			total, err := strconv.Atoi(cubeData[0])
+			if err != nil {
+				return 0, err
+			}
+
+			colour := cubeData[1]
+
+			switch {
+			case colour == "red" && total > maxRedTotal:
+				maxRedTotal = total
+			case colour == "green" && total > maxGreenTotal:
+				maxGreenTotal = total
+			case colour == "blue" && total > maxBlueTotal:
+				maxBlueTotal = total
+			}
+		}
+	}
+
+	return maxRedTotal * maxGreenTotal * maxBlueTotal, nil
 }
 
 type puzzleSolver struct{}
@@ -37,10 +68,6 @@ func (solver *puzzleSolver) GetDay() int {
 }
 
 func (solver *puzzleSolver) SolvePuzzle(puzzleNumber puzzle.Number, input *bufio.Scanner) (int, error) {
-	if puzzleNumber == 2 {
-		return 0, nil
-	}
-
 	sum := 0
 
 	for input.Scan() {
@@ -55,13 +82,22 @@ func (solver *puzzleSolver) SolvePuzzle(puzzleNumber puzzle.Number, input *bufio
 
 		cubeSets := strings.Split(gameData[1], "; ")
 
-		isGamePossible, err := isGamePossible(cubeSets)
-		if err != nil {
-			return 0, err
+		var handleCubeSets func([]string) (int, error)
+		switch puzzleNumber {
+		case puzzle.One:
+			handleCubeSets = func(cubeSets []string) (int, error) {
+				return passThroughValueIfGameIsPossible(cubeSets, id)
+			}
+		case puzzle.Two:
+			handleCubeSets = calculatePowerOfCubes
+		default:
+			return 0, errors.New(fmt.Sprintf("Invalid puzzle number: %v.", puzzleNumber))
 		}
 
-		if isGamePossible {
-			sum += id
+		if result, err := handleCubeSets(cubeSets); err != nil {
+			return 0, err
+		} else {
+			sum += result
 		}
 	}
 
